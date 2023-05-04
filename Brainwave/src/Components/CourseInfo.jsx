63aeda42f25c22e37610a19961847d7728c128f7
André Oliveira <andreaoliveira@ua.pt>
2023-05-04 19:50:38 +0100

    Update CourseInfo.jsx

--- Brainwave/src/Components/CourseInfo.jsx
+++ Brainwave/src/Components/CourseInfo.jsx
@@ -26,41 +26,47 @@ function CourseInfo() {
                 <div className="flex justify-between items-center mb-4">
                 <h3 className="text-xl font-bold">Disciplina</h3>
                 <h3 className="text-xl font-bold">Avaliação</h3>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={openModal}>
+                <label htmlFor="modal" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     Comprar
-                </button>
+                </label>
                 </div>
                 <p className="mb-4">Descrição do Curso</p>
             </div>
-            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="modal" overlayClassName="overlay">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h2 className="modal-title">Pagamento</h2>
-                        <button className="modal-close" onClick={closeModal}>X</button>
-                    </div>
-                    <div className="modal-body">
-                        <div className="flex flex-col">
-                            <label htmlFor="card-number">Número do Cartão de Crédito:</label>
-                            <input type="text" id="card-number" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
-                            <label htmlFor="expiry-date">Data de Expiração:</label>
-                            <input type="text" id="expiry-date" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
-                            <label htmlFor="cvv">CVV:</label>
-                            <input type="text" id="cvv" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
-                                Comprar
-                            </button>
-                            <p className="text-center">ou</p>
-                            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                                Pagar com PayPal
-                            </button>
+
+            {/* Modal Pagamento */}
+            <input type="checkbox" id="modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h2 className="modal-title">Pagamento</h2>
+                            <button className="modal-close" onClick={closeModal}>X</button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="flex flex-col">
+                                <label htmlFor="card-number">Número do Cartão de Crédito:</label>
+                                <input type="text" id="card-number" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
+                                <label htmlFor="expiry-date">Data de Expiração:</label>
+                                <input type="text" id="expiry-date" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
+                                <label htmlFor="cvv">CVV:</label>
+                                <input type="text" id="cvv" className="border border-gray-300 rounded-lg px-4 py-2 mb-4" />
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+                                    Comprar
+                                </button>
+                                <p className="text-center">ou</p>
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                    Pagar com PayPal
+                                </button>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <h3 className="modal-summary">Resumo da Compra</h3>
+                            <div className="modal-price">$100</div>
                         </div>
                     </div>
-                    <div className="modal-footer">
-                        <h3 className="modal-summary">Resumo da Compra</h3>
-                        <div className="modal-price">$100</div>
-                    </div>
                 </div>
-            </Modal>
+            </div>
         </>
   );
 }
